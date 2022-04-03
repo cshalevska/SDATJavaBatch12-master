264c42e1e99cefb274abcb6593b8f9f6b4a2c2dd
Cece Shalevska <cshalevska@gmail.com>
2022-04-03 12:35:16 -0400

    Initial commit

--- src/Class30/MapDemo4.java
+++ src/Class30/MapDemo4.java
@@ -11,13 +11,14 @@ public class MapDemo4 {
         fruitMap.put("Kiwi",105.2);
         fruitMap.put("Oranges",16.5);
         fruitMap.put("Mango",20.2);
-
+// getting the interator from the map with the help of method chainging
+        //fist we are getting the set from the map that we are getting the iterator on that set
        Iterator<String> iterator= fruitMap.keySet().iterator();
-       while (iterator.hasNext()){
-           String key=iterator.next();
-           Double value=fruitMap.get(key);
+       while (iterator.hasNext()){ // check ifthere are still elements left in the itarator
+           String key=iterator.next(); // getting the key from the iterator
+           Double value=fruitMap.get(key); // getting the corresponding value from the map
            if(key.contains("e")&& value>=20){
-               iterator.remove();
+               iterator.remove();// if both the condition are satisfied we remove the elements
            }
        }
         System.out.println(fruitMap);
